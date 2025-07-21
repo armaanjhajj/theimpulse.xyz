ce0dae9534a2140f877226c2a1c10a9f9cdb8a29
Armaan Jhajj <166579534+armaanjhajj@users.noreply.github.com>
2025-07-21 08:46:43 -0400

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client"; // ✅ correct import
-import App from "./App";
-
-const root = createRoot(document.getElementById("root")); // ✅ new root
-root.render(<App />);
